ae22f2ec9783d165963d9d229e148260b1318f40
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-02-25 19:49:37 -0600

    completed stretch!

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -8,55 +8,68 @@
 */
 
 class Carousel {
-    constructor(carElement){
-        this.carElement = carElement;
-        console.log(this.carElement);
+  constructor(carElement){
+      this.carElement = carElement;
 
-        this.images = carElement.querySelectorAll('img'); // grab a reference to all of the images
-        console.log(this.images);
+      this.images = carElement.querySelectorAll('.carousel img'); // grab a reference to all of the images
+      // console.log(this.images);
 
-        this.currentIndex = 2; // set index to 2
+      this.currentIndex = 0; // set index to 0
 
-        this.leftButton = carElement.querySelector('.left-button');
+      this.leftButton = carElement.querySelector('.left-button'); 
 
-        this.rightButton = carElement.querySelector('.right-button');
+      this.rightButton = carElement.querySelector('.right-button');
 
-        this.leftButton.addEventListener('click', () => {
-          this.leftButtonClicked();
-          this.currentIndex--;
-          this.displayImg(this.currentIndex);
-        });
+      this.leftButton.addEventListener('click', () => {
+        this.leftButtonClicked();
+        console.log(this.currentIndex);
+      });
 
-        this.rightButton.addEventListener('click', () => {
-          this.rightButtonClicked();
-          this.currentIndex++;
-          this.displayImg(this.currentIndex);
-        });
+      this.rightButton.addEventListener('click', () => {
+        this.rightButtonClicked();
+        console.log(this.currentIndex);
+      });
 
+  }
+
+  leftButtonClicked(){
+    console.log('left button clicked');
+    this.images[this.currentIndex].style.display = 'none';
+
+    // checks current index if it is greater than 0. If it is, then subtracts 1, sets display to block
+    if(this.currentIndex > 0) {
+      this.currentIndex -= 1;
+      this.images[this.currentIndex].style.display = 'block';
     }
-
-    leftButtonClicked(){
-      console.log('left button clicked');
-      this.carElement.classList.toggle('left-button');
-      console.log(this.currentIndex);
-      
+    // change current index to 3
+    else {
+      this.currentIndex = 3;
+      this.images[this.currentIndex].style.display = 'block';
     }
+  }
 
-    rightButtonClicked(){
-      console.log('right button clicked');
-      this.carElement.classList.toggle('right-button');
-      console.log(this.currentIndex);
-    }
+  rightButtonClicked(){
+    console.log('right button clicked');
+    this.images[this.currentIndex].style.display = 'none';
 
-    displayImg(element){
-      this.currentIndex = element;
-      this.display = this.images[this.currentIndex];// display current img
-      this.display.style.display = 'flex'; //display item
-      console.log(this.display);
+    // checks current index if it is less than 3. If it is, then add 1, sets display to block
+    if(this.currentIndex < 3) {
+      this.currentIndex += 1;
+      this.images[this.currentIndex].style.display = 'block';
     }
+    // change current index to 0
+    else {
+      this.currentIndex = 0;
+      this.images[this.currentIndex].style.display = 'block';
+    }
+  }
 
 }
 
-let carousels = document.querySelectorAll('.carousel');
+let carousel = document.querySelector('.carousel');
 
-carousels.forEach( car => new Carousel(car));
+carousel = new Carousel(carousel);
+
+// console.log(carousel);
+
+carousel.images[0].style.display = 'block'; //sets picture on load
